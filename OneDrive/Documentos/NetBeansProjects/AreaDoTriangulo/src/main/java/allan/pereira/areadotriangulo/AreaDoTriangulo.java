31ae62518c50c03b957af593dc731d09b2dfa122
Allan Pereira <allan.m.pereira@outlook.com>
2023-03-02 22:27:05 -0300

    git remote add origin https://github.com/AllanMatheusPereira/JAVA.git git branch -M main git push -u origin main git status git commit -m 'JAVA'

---
+++ OneDrive/Documentos/NetBeansProjects/AreaDoTriangulo/src/main/java/allan/pereira/areadotriangulo/AreaDoTriangulo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package allan.pereira.areadotriangulo;
+import java.util.Scanner;
+
+/**
+ *
+ * @author allan
+ */
+public class AreaDoTriangulo {
+    
+    public static void main(String[] args) {
+        
+        Scanner ler = new Scanner (System.in);
+        System.out.println("Qual a altura do triangulo:");
+        int numero = ler.nextInt();
+        System.out.println("Qual a base:");
+        int base = ler.nextInt();
+        int valor = (numero * base) / 2;
+        if (valor > 10) {
+            System.out.println("Hello World");
+        }else{
+            System.out.println("...");
+        }
+        System.out.println("A area do triangulo é: " + valor);
+    }
+}
